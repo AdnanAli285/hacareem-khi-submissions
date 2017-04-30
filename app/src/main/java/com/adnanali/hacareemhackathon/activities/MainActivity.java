c25b5f9b27814e894b373cd3701e7bdb4ae91cf0
AdnanAli285 <adnanaliseven86@outlook.com>
2017-04-30 10:31:13 +0500

    Solution submission for problem 01

---
+++ app/src/main/java/com/adnanali/hacareemhackathon/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.adnanali.hacareemhackathon.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.adnanali.hacareemhackathon.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
